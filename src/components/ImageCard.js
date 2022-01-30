1e3d18f914d5bb2be36136b9d4a3fa6f77768cd1
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2022-01-31 00:39:13 +0700

    Update Code

---
+++ src/components/ImageCard.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@mui/material';
+
+function ImageCard() {
+	const bull = (
+		<Box component="span" sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
+			•
+		</Box>
+	);
+	return (
+		<Box
+			component="div"
+			sx={{
+				display: 'center',
+				justifyContent: 'center',
+				pt: '10%',
+			}}>
+			<Card
+				component="div"
+				sx={{
+					minWidth: 275,
+					width: 1 / 2,
+				}}>
+				<CardContent>
+					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+						Word of the Day
+					</Typography>
+					<Typography variant="h5" component="div">
+						be{bull}nev{bull}o{bull}lent
+					</Typography>
+					<Typography sx={{ mb: 1.5 }} color="text.secondary">
+						adjective
+					</Typography>
+					<Typography variant="body2">
+						well meaning and kindly.
+						<br />
+						{'"a benevolent smile"'}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button size="small">Learn More</Button>
+				</CardActions>
+			</Card>
+		</Box>
+	);
+}
+
+export default ImageCard;
